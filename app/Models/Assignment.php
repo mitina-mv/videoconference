c014bd14cfa7bcd0f8220fe468105545a900e7cf
Maria <mit.maria2019@yandex.ru>
2024-05-13 07:35:37 +0300

    отредактировала модели

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -2,10 +2,43 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Assignment extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'test_id',
+        'date',
+    ];
+    protected $table = 'assignments';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'date' => 'datetime',
+    ];
+
+    
+    public function getDateAttribute($value)
+    {
+        return Carbon::parse($value)->format('d.m.Y H:i');
+    }
+
+    public function test()
+    {
+        return $this->belongsTo(Test::class);
+    } 
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Testlog.php
+++ app/Models/Testlog.php
@@ -15,27 +15,18 @@ class Testlog extends Model
     public $timestamps = true;
 
     protected $fillable = [
-        'date',
         'mark',
         'time',
         'user_id',
-        'test_id',
-        'teacher_id',
         'uncorrect_answers',
     ];
 
     protected $casts = [
         'uncorrect_answers' => 'array',
         'created_at' => 'datetime',
-        'date' => 'datetime',
     ];
 
     public function getCreatedAtAttribute($value)
-    {
-        return Carbon::parse($value)->format('d.m.Y H:i');
-    }
-
-    public function getDateAttribute($value)
     {
         return Carbon::parse($value)->format('d.m.Y H:i');
     }
@@ -43,11 +34,6 @@ class Testlog extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
-    }
-
-    public function test()
-    {
-        return $this->belongsTo(Test::class, 'test_id', 'id');
     }
 
     public function answerlogs()
