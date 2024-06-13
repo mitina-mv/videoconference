390ac1a1c0b324bb11f355dd37c3d4af2fd5dd5d
Maria <mit.maria2019@yandex.ru>
2024-06-13 09:45:58 +0300

    feat: отчет о тестировании и для вк студента

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,7 +10,8 @@ class ReportController extends Controller
     {
         $testlog = Testlog::where('id', $testlog_id)
             ->with([
-                'assignment', 
+                'assignment',
+                'assignment.videoconference',
                 'assignment.user', 
                 'answerlogs', 
                 'answerlogs.answers', 
@@ -26,13 +27,36 @@ class ReportController extends Controller
             return $this->renderError('Reports/Student', 'Вы еще не проходили этот тест');
         }
 
-        // dd($testlog->assignment->toArray());
+        $questions = [];
+        foreach($testlog->answerlogs->toArray() as $q)
+        {
+            $item = [
+                'mark' => $q['mark'],
+                'total_mark' => $q['question']['mark'],
+                'question' => $q['question']['text'],
+                'answers' => empty($q['answers']) ? null : array_column($q['answers'], 'history')
+            ];
+            if($q['question']['type'] == 'text'
+                && empty($q['answers'])
+                && $testlog->uncorrect_answers
+            ) {
+                if(isset($testlog->uncorrect_answers[$q['question']['id']])) {
+                    $item['answers'][] = [
+                        "is_correct" => false,
+                        "text" => $testlog->uncorrect_answers[$q['question']['id']]
+                    ];
+                }
+            }
+            $questions[] = $item;
+        }
 
         return Inertia::render('Reports/Student', [
             'test' => $testlog->assignment->test()->select('name')->first(),
             'testlog' => $testlog,
             'user' => request()->user(),
-            'questions' => $testlog->answerlogs
+            'questions' => $questions,
+            'vc' => $testlog->assignment->videoconference,
+            'backLink' => 'assignments.my'
         ]);
     }
 
--- resources/js/Pages/Reports/Student.vue
+++ resources/js/Pages/Reports/Student.vue
@@ -2,12 +2,14 @@
 import { ref, onMounted } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head, usePage } from "@inertiajs/vue3";
+import labels from "@/locales/ru.js";
 
 const props = defineProps({
     test: [Object, null],
     error: [String, null],
     testlog: [Object, null],
     questions: [Array, null],
+    vc: [Object, null],
     user: Object,
 });
 
@@ -25,18 +27,70 @@ const testInfoFields = [
         value: props.testlog.assignment.date,
     },
     {
-        label: "Проводит",
+        label: "Преподаватель",
         value: props.testlog.assignment.user.full_name,
     },
-    {},
+    {
+        label: "Оценка",
+        value: props.testlog.mark,
+    },
 ];
 </script>
 
 <template>
-    <h2>Результаты тестирования: {{ test.name }}</h2>
-    <div class="test-info">
-        <p v-for="(field, index) in testInfoFields" :key="index">
-            <b>{{ field.label }}: </b> <span>{{ field.value }}</span>
-        </p>
-    </div>
+    <Head :title="`Результаты тестирования: ${test.name}`" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Результаты тестирования: {{ test.name }}
+            </h2>
+        </template>
+
+        <div class="d-grid gap-4 content">
+            <div class="content__container report report_student">
+                <!-- <h2>Результаты тестирования: {{ test.name }}</h2> -->
+                <div class="test-info">
+                    <p v-for="(field, index) in testInfoFields" :key="index">
+                        <b>{{ field.label }}: </b>
+                        <span>{{ field.value }}</span>
+                    </p>
+                </div>
+                <div class="vc-info" v-if="vc">
+                    <p><b>Видеоконференция: </b>{{ vc.name }}</p>
+                    <p><b>Дата конференции: </b>{{ vc.date }}</p>
+                </div>
+                <table class="mt-3">
+                    <thead>
+                        <tr>
+                            <th>Вопрос</th>
+                            <th>Ваш ответ</th>
+                            <th>Стоимость</th>
+                            <th>Оценка</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(q, ind) in questions" :key="ind">
+                            <td width="35%">{{ q.question }}</td>
+                            <td width="45%">
+                                <span
+                                    v-for="(a, i) in q.answers"
+                                    :key="ind + '_' + i"
+                                    :class="
+                                        a.is_correct
+                                            ? 'text-success'
+                                            : 'text-danger'
+                                    "
+                                >
+                                    {{ a.text }}
+                                </span>
+                            </td>
+                            <td width="10%">{{ Number(q.total_mark) }}</td>
+                            <td width="10%">{{ Number(q.mark) }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </AuthenticatedLayout>
 </template>
